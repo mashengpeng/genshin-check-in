ef7620ffb8353de6160c60573fe25a1e2bf848b8
mashengpeng <1360311705@qq.com>
2022-09-25 15:22:51 +0800

    fix：编码

--- index.py
+++ index.py
@@ -61,19 +61,21 @@ def check_in(cookie_string):
     game_info = get_game_info(cookies)
     if game_info['retcode'] != 0:
         print("Failed to get game info")
-        print(str(game_info).encode("utf-8"))
+        print(str(game_info))
     else:
         print("Successfully obtained game information")
-        print(str(game_info).encode("utf-8"))
+        print(str(game_info))
         check_in_res = bbs_sign_reward(cookies, get_ds(), game_info)
         if check_in_res['retcode'] == 0:
             print("Sign in successfully")
-            print(str(check_in_res).encode("utf-8"))
+            print(str(check_in_res))
         else:
             print("Failed to sign in")
-            print(str(check_in_res).encode("utf-8"))
+            print(str(check_in_res))
 
 
 # python index.py "_MHYUUID=bd20878a-1d35-457c-b84c-9ea929ced396; _ga=GA1.2.128251121.1653128272; _gid=GA1.2.321587272.1653128272; _gat=1"
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
 cookie = os.environ["GENSHIN_COOKIE"]
 check_in(cookie)
