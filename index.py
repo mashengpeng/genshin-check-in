2fcc411734185c25ec33642852e8e52400bfcf40
mashengpeng <1360311705@qq.com>
2022-09-25 13:58:31 +0800

    fix:使用secret

--- index.py
+++ index.py
@@ -53,20 +53,24 @@ def bbs_sign_reward(cookies, ds, game_info):
 
 
 def check_in(cookie_string):
+    print(cookie_string)
     cookies = cookie_str2dict(cookie_string)
     game_info = get_game_info(cookies)
-    if (game_info['retcode'] != 0):
+    if game_info['retcode'] != 0:
         print("Failed to get game info")
         print(game_info)
     else:
         print("Successfully obtained game information")
-        # print(game_info)
+        print(game_info)
         check_in_res = bbs_sign_reward(cookies, get_ds(), game_info)
-        if(check_in_res['retcode'] == 0):
+        if check_in_res['retcode'] == 0:
             print("Sign in successfully")
+            print(check_in_res)
         else:
             print("Failed to sign in")
             print(check_in_res)
 
+
 # python index.py "_MHYUUID=bd20878a-1d35-457c-b84c-9ea929ced396; _ga=GA1.2.128251121.1653128272; _gid=GA1.2.321587272.1653128272; _gat=1"
-check_in(sys.argv[1])
+cookie = os.environ["GENSHIN_COOKIE"]
+check_in(cookie)
